f8aeb4716cb59025d84fcd4f30ea4ce67c0b08ed
Enaium <wyy-666@hotmail.com>
2022-01-28 13:06:59 +0800

    Add argument

--- src/main/java/net/minecraft/launchwrapper/template/TemplateTweaker.java
+++ src/main/java/net/minecraft/launchwrapper/template/TemplateTweaker.java
@@ -2,16 +2,30 @@ package net.minecraft.launchwrapper.template;
 
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraft.launchwrapper.LogWrapper;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TemplateTweaker implements ITweaker {
-    private List<String> args;
+    private final List<String> arguments = new ArrayList<>();
 
     @Override
     public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {
-        this.args = args;
+        this.arguments.addAll(args);
+        addArgument("--gameDir", gameDir != null ? gameDir.getAbsolutePath() : null);
+        addArgument("--assetsDir", assetsDir != null ? assetsDir.getPath() : null);
+        addArgument("--version", profile);
+    }
+
+    public void addArgument(String name, String value) {
+        if (value != null) {
+            arguments.add(name);
+            arguments.add(value);
+        } else {
+            LogWrapper.severe("Argument %s is %s", name, null);
+        }
     }
 
     @Override
@@ -26,6 +40,6 @@ public class TemplateTweaker implements ITweaker {
 
     @Override
     public String[] getLaunchArguments() {
-        return args.toArray(new String[0]);
+        return arguments.toArray(new String[0]);
     }
 }
